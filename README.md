829023b64a1e507c6817fe5f3c0b2dc43f3f0b99
Danylo <rdv.romanov@gmail.com>
2022-07-13 22:23:13 +0300

    Reactapp

--- README.md
+++
@@ -1,2 +0,0 @@
-# ReactStatic
-React static pages
---
+++ static/src/Training.js
@@ -0,0 +1,9 @@
+function TrainComponent(){
+    return (
+        <h1>
+            Test string
+            sdsds
+        </h1>
+    )
+}
+export default TrainComponent
