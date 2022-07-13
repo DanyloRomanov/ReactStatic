1840dd2cc1c500c6edac6709701fb7de957b8fb3
Danylo <rdv.romanov@gmail.com>
2022-07-13 18:08:48 +0300

    Why composable

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
+function MainContent( ){
+    return (
+        <ul>
+            <li>Item1</li>
+            <li>Item2</li>
+        </ul>
+    )
+}
+
 const container = document.getElementById("root1")
 
 const root = ReactDOM.createRoot(container);
 root.render(
-    <h1>
+    <div>
+        <h1>
         Hello smthn
-    </h1>
+        </h1>
+        <MainContent/>
+    </div>
 )
